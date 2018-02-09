57f50ad6093aa7606ffead0162f1691ed90969ae
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-02-09 13:43:37 +0000

    call back errors

--- frontend/build/bundle.js
+++ frontend/build/bundle.js
@@ -69,12 +69,18 @@
 
 const NasaAPI = __webpack_require__(1);
 
+
+const doSomethingWithData = function(data) {
+    console.log(data)
+}
+
 const app = function () {
     var venusURL = 'https://images-api.nasa.gov/search?media_type=video&keywords=venus'
     var venusVideoURL = 'https://images-assets.nasa.gov/video/JPL-19621214-MARINRf-0001-AVC2002150 First Flyby of Another Planet Mariner 2/collection.json'
     var video1 = 'https://images-assets.nasa.gov/video/JPL-19621214-MARINRf-0001-AVC2002150%20First%20Flyby%20of%20Another%20Planet%20Mariner%202/collection.json'
     var nasaAPI = new NasaAPI(venusURL);
-    nasaAPI.getCollectionURLS();
+    nasaAPI.getCollectionURLS(doSomethingWithData);
+    console.log(nasaAPI.colllectionURLs);
 
     var testVideo = document.querySelector('#test-video');
 
@@ -87,23 +93,42 @@ window.addEventListener('load', app);
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Request = __webpack_require__(2);
+const Requests = __webpack_require__(2);
 
 const NasaAPI = function (url) {
     this.url = url;
-    this.colllectionURLS = [];
+    this.colllectionURLs = [];
 }
 
 NasaAPI.prototype.getCollectionURLS = function () {
-    var request = new Request(this.url);
-    request.getRequest()
+    var request = new Requests(this.url);
+    request.getRequest(this.getHrefs.bind(this))
 }
 
 
 NasaAPI.prototype.getHrefs = function (searchResults) {
-    var hrefs = videos.map(searchResults = searchResults => [searchResults.collection.items.href]);
-    return hrefs;
+    var items = searchResults.collection.items;
+    var hrefs = []
+    items.forEach(function (item) {
+        hrefs.push(item.href);
+    }.bind(this))
+    console.log(hrefs);
 }
+
+// NasaAPI.prototype.getJSONData = function (hrefs) {
+//     hrefs.forEach(function (url) {
+//         var request = new Requests(url)
+//         request.getRequest(this.seeCollection.bind);
+//     })
+// }
+//
+// NasaAPI.prototype.seeCollection= function(data){
+//     console.log(data)
+// }
+
+// NasaAPI.prototype.setCollectionURLs = function (hrefs) {
+//     this.colllectionURLs = hrefs;
+// }.bind(this);
 
 // var unconvertedString = searchResults.collection.items[0].href;
 // var changedString = unconvertedString.replace(/ /g,"%20");
--- frontend/src/models/nasaAPI.js
+++ frontend/src/models/nasaAPI.js
@@ -1,20 +1,39 @@
-const Request = require('../services/requests');
+const Requests = require('../services/requests');
 
 const NasaAPI = function (url) {
     this.url = url;
-    this.colllectionURLS = [];
+    this.colllectionURLs = [];
 }
 
 NasaAPI.prototype.getCollectionURLS = function () {
-    var request = new Request(this.url);
-    request.getRequest()
+    var request = new Requests(this.url);
+    request.getRequest(this.getHrefs.bind(this))
 }
 
 
 NasaAPI.prototype.getHrefs = function (searchResults) {
-    var hrefs = videos.map(searchResults = searchResults => [searchResults.collection.items.href]);
-    return hrefs;
+    var items = searchResults.collection.items;
+    var hrefs = []
+    items.forEach(function (item) {
+        hrefs.push(item.href);
+    }.bind(this))
+    console.log(hrefs);
 }
+
+// NasaAPI.prototype.getJSONData = function (hrefs) {
+//     hrefs.forEach(function (url) {
+//         var request = new Requests(url)
+//         request.getRequest(this.seeCollection.bind);
+//     })
+// }
+//
+// NasaAPI.prototype.seeCollection= function(data){
+//     console.log(data)
+// }
+
+// NasaAPI.prototype.setCollectionURLs = function (hrefs) {
+//     this.colllectionURLs = hrefs;
+// }.bind(this);
 
 // var unconvertedString = searchResults.collection.items[0].href;
 // var changedString = unconvertedString.replace(/ /g,"%20");
