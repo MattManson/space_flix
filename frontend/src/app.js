4e401246be6a2cea83ba8b27226962799fd00e34
catrionameriel <catriona.meriel@gmail.com>
2018-02-11 14:24:05 +0000

    made visible invisible buttons work

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,7 +3,15 @@ const VideoView = require('./views/videoView.js');
 const Apod = require('./models/apod.js');
 
 const SoundNASAData = require('./models/SoundNASAData');
-const ApiKey = require('./API_key')
+const ApiKey = require('./API_key');
+
+let buttonClicked = function () {
+  let frontPageContainer = document.querySelector('#front-page');
+  frontPageContainer.className = 'invisible';
+  let viewPageContainer= document.querySelector('#view-page');
+  viewPageContainer.className = 'visible';
+
+}
 
 const app = function () {
     var venusURL = 'https://images-api.nasa.gov/search?media_type=video&keywords=space'
@@ -11,9 +19,7 @@ const app = function () {
     var apodurl = 'https://api.nasa.gov/planetary/apod?api_key=9AsiGWIMkVlJVOoljVmpT2mNvJNFPHSL1ZdTa74k'
 
     var apod = new Apod(apodurl);
-    apod.getImage()
-    // apod.imagesForSlider();
-    // apod.makeSlider();
+    apod.getImage();
 
     var nasaAPI = new NasaAPI(venusURL);
     nasaAPI.onLoad = videoView.render.bind(videoView);
@@ -25,6 +31,11 @@ const app = function () {
     var mattKey = new ApiKey().getMattKey();
     var soundNasaApi = new SoundNASAData('https://api.nasa.gov/planetary/sounds?q=mars&api_key=' + mattKey);
     soundNasaApi.getData();
+
+    let buttons = document.querySelectorAll('button');
+    for(button of buttons) {
+      addEventListener('click', buttonClicked)
+    };
 }
 
 window.addEventListener('load', app);
--- frontend/src/models/apod.js
+++ frontend/src/models/apod.js
@@ -10,10 +10,6 @@ Apod.prototype.displayImage = function(imageDetails){
   var imageLocation = document.querySelector('#apod-slider');
   var apodImage = document.createElement('img');
   apodImage.src = imageDetails.hdurl;
-  if(apodImage.height> apodImage.width) {
-    apodImage.height = '100%';
-    apodImage.width = 'auto';
-  }
   imageLocation.appendChild(apodImage);
 }
 
@@ -23,43 +19,3 @@ Apod.prototype.getImage = function(){
 }
 
 module.exports = Apod;
-
-// Apod.prototype.getImages = function(){
-//   var request = new Request(this.url);
-//   request.getRequest(this.fillArray.bind(this))
-// }
-//
-// Apod.prototype.fillArray = function(info){
-//     this.recentImages = info;
-// }
-//
-// Apod.prototype.imagesForSlider = function(){
-//   var imageLocation = document.querySelector('#apod-slider');
-//   this.recentImages.forEach(function(element){
-//     console.log(element);
-//     var li = document.createElement('li')
-//     var image = document.createElement('img');
-//     image.src = element.hdurl;
-//     // image.className = "sliderImage"
-//     imageLocation.appendChild(image);
-//   })
-// }
-
-
-//
-// var slideIndex = 0;
-// // carousel();
-//
-// Apod.prototype.makeSlider = function() {
-//   var ul;
-//   var liItems;
-//   var imageWidth;
-//   var imageNumber;
-//   ul = document.querySelector('#apod-slider');
-//   liItems = ul.children;
-//   imageNumber = liItems.length;
-//   imageWidth = liItems[0].children[0].offsetWidth;
-//   // set ul’s width as the total width of all images in image slider.
-//   ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-//   slider(ul);
-// }
