6eec5d69d8ef81338dc171f6f5c90000cc8f0383
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-02-11 14:55:13 +0000

    buttons made dynamic

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -5,37 +5,37 @@ const Apod = require('./models/apod.js');
 const SoundNASAData = require('./models/SoundNASAData');
 const ApiKey = require('./API_key');
 
-let buttonClicked = function () {
-  let frontPageContainer = document.querySelector('#front-page');
-  frontPageContainer.className = 'invisible';
-  let viewPageContainer= document.querySelector('#view-page');
-  viewPageContainer.className = 'visible';
-
-}
-
 const app = function () {
-    var venusURL = 'https://images-api.nasa.gov/search?media_type=video&keywords=space'
+    var videosURL = 'https://images-api.nasa.gov/search?media_type=video&keywords='
     var videoView = new VideoView(document.querySelector('#test-videos'));
     var apodurl = 'https://api.nasa.gov/planetary/apod?api_key=9AsiGWIMkVlJVOoljVmpT2mNvJNFPHSL1ZdTa74k'
 
     var apod = new Apod(apodurl);
     apod.getImage();
 
-    var nasaAPI = new NasaAPI(venusURL);
-    nasaAPI.onLoad = videoView.render.bind(videoView);
-    nasaAPI.getCollectionURLS();
-
-
-
     var catrionaKey = new ApiKey().getCatrionaKey();
     var mattKey = new ApiKey().getMattKey();
     var soundNasaApi = new SoundNASAData('https://api.nasa.gov/planetary/sounds?q=mars&api_key=' + mattKey);
     soundNasaApi.getData();
 
+    let buttonClicked = function () {
+        console.log(this.id);
+        var nasaAPI = new NasaAPI(videosURL + this.id);
+        nasaAPI.onLoad = videoView.render.bind(videoView);
+        nasaAPI.getCollectionURLS();
+        let frontPageContainer = document.querySelector('#front-page');
+        frontPageContainer.className = 'invisible';
+        let viewPageContainer= document.querySelector('#view-page');
+        viewPageContainer.className = 'visible';
+
+    }
+
+    var counter = 1;
     let buttons = document.querySelectorAll('button');
-    for(button of buttons) {
-      addEventListener('click', buttonClicked)
-    };
+    for ( var counter = 0; counter < buttons.length; counter++)
+    {
+        buttons[counter].addEventListener("click",buttonClicked );
+    }
 }
 
 window.addEventListener('load', app);
