bd8e0f1f312d3d027c64188328bf9ce71146ebaf
Matt <matthew.r.manson@gmail.com>
2018-02-11 15:50:31 +0000

    search function working, still 2 constant videos

--- frontend/src/views/videoView.js
+++ frontend/src/views/videoView.js
@@ -1,21 +1,25 @@
 const VideoView = function (container) {
-    this.container = container;
+  this.container = container;
 }
 
 VideoView.prototype.render = function (data) {
-        var correctVideoURL = data[0].replace(/ /g,"%20");
-        var correctThumbNailURL = data[data.length-2].replace(/ /g,"%20");
-        // console.log(correctThumbNailURL);
-        var img = document.createElement('img');
-        img.width = 320;
-        img.height = 240;
-        img.src = correctThumbNailURL;
-        img.onclick = function() {
-            window.location.href = correctVideoURL;
-        };
-        this.container.appendChild(img);
+  var substring = "mov";
+  var correctVideoURL = data[0].replace(/ /g,"%20");
+  console.log(correctVideoURL);
+  if (!correctVideoURL.includes(substring)){
+    var correctThumbNailURL = data[data.length-2].replace(/ /g,"%20");
+    // console.log(correctThumbNailURL);
+    var img = document.createElement('img');
+    img.src = " "
+    img.width = 320;
+    img.height = 240;
+    img.src = correctThumbNailURL;
+    img.onclick = function() {
+      window.location.href = correctVideoURL;
+    };
+    this.container.appendChild(img);
+  }
 }
-
 VideoView.prototype.clear = function(){
   this.container.innerText = " ";
 }
