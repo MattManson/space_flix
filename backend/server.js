2fdf8dfe6cca5c96f014b9f55b88d93183decb88
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-02-13 10:22:39 +0000

    can get and post to database

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,50 @@ server.use(cors());
 
 server.use(parser.json());
 server.use(parser.urlencoded({extended:true}));
+
+MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    const db = client.db('favourites');
+    console.log('Connected to favourites database');
+
+    //GET ALL
+
+    server.get('/api/favourites', function (req, res) {
+        db.collection('videos').find().toArray(function (err, result) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.send();
+            }
+
+            res.status(200)
+            res.json(result);
+            console.log("Database retrieved");
+        });
+    });
+
+    //POST
+
+    server.post('/api/favourites', function (req, res) {
+        db.collection('videos').save(req.body, function (err, result) {
+            if(err){
+                console.log(err);
+                res.status(500);
+                res.send();
+            }
+            res.status(201);
+            res.json(result.ops[0]);
+
+            console.log("Object Saved")
+        })
+    })
+
+    server.listen(5000, function () {
+        console.log('Listening on port 5000');
+    })
+
+})
