c79788111a003e7110bd987edcf4a1068c2763c3
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-02-13 09:43:18 +0000

    server/database started

---
+++ backend/seeds.js
@@ -0,0 +1,19 @@
+use favourites;
+
+db.videos.insert({
+    title: "JPL-19621214-MARINRf-0001-AVC2002150 First Flyby of Another Planet Mariner 2",
+    description: "Mariner 2's flyby of Venus on Dec. 14, 1962, was the first successful robotic spacecraft encounter with another planet. Includes animation.",
+    href: "http://images-assets.nasa.gov/video/JPL-19620827-MARINRf-0001-AVC2002150 Mariner 2 Launches/JPL-19620827-MARINRf-0001-AVC2002150 Mariner 2 Launches~orig.mp4",
+    thumbnail: "http://images-assets.nasa.gov/video/JPL-19620827-MARINRf-0001-AVC2002150 Mariner 2 Launches/JPL-19620827-MARINRf-0001-AVC2002150 Mariner 2 Launches~small_thumb_00001.png"});
+
+db.videos.insert({
+    title: "JPL-19620827-MARINRf-0001-AVC2002150 Mariner 2 Launches",
+    description: "The launch of Mariner 2, which flew by Venus and was the first robotic space probe to conduct a successful planetary encounter.",
+    href: "http://images-assets.nasa.gov/video/158_BeautifulConjunction/158_BeautifulConjunction~orig.mp4",
+    thumbnail: "http://images-assets.nasa.gov/video/158_BeautifulConjunction/158_BeautifulConjunction~small_thumb_00003.png"});
+
+db.videos.insert({
+    title: "ScienceCast 184: Amazing Sunset Sky Show",
+    description: "Venus and Jupiter are converging for a must-see close encounter at the end of June. It could be the best backyard sky show of 2015.",
+    href: "http://images-assets.nasa.gov/video/226_SpactacularConjunction/226_SpactacularConjunction~orig.mp4",
+    thumbnail: "http://images-assets.nasa.gov/video/226_SpactacularConjunction/226_SpactacularConjunction~small_thumb_00003.png"});
---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const parser = require('body-parser');
+const server = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const cors = require('cors');
+server.use(cors());
+
