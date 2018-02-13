7bf43ef4b63fc7694970fe6c38b5e1a0c29f2e79
catrionameriel <catriona.meriel@gmail.com>
2018-02-13 09:49:17 +0000

    deleted API_key

--- frontend/src/API_key.js
+++
@@ -1,9 +0,0 @@
-const APIKey = function(){
-  this.key = 'QLn3BOptgkNzClciQuNWXzwV0AsUVKOCr01MbgFk';
-};
-
-APIKey.prototype.getKey = function () {
-  return this.key;
-};
-
-module.exports = APIKey;
