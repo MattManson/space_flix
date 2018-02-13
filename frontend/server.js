62512f324ace8e5dc07cefd1b39295554a954486
catrionameriel <catriona.meriel@gmail.com>
2018-02-13 11:24:11 +0000

    added icon

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 const request = require('request');
 server.use(express.static('build'));
+server.use(express.static('public'));
 
 server.get('/api/nasa/planetary/sounds/:planet',function(req,res) {
   const url = `https://api.nasa.gov/planetary/sounds?q=${req.params.planet}&api_key=QLn3BOptgkNzClciQuNWXzwV0AsUVKOCr01MbgFk`;
