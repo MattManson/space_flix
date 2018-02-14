e084eb2e247a1fd08371250d3e6614e888e424ef
Matt <matthew.r.manson@gmail.com>
2018-02-14 10:44:42 +0000

    indentation

--- frontend/src/models/favourites.js
+++ frontend/src/models/favourites.js
@@ -1,33 +1,33 @@
 const Request = require('../services/requests.js');
 
 const Favourites = function () {
-    this.url = 'http://localhost:5000/api/favourites'
-    this.onLoad = null;
+  this.url = 'http://localhost:5000/api/favourites'
+  this.onLoad = null;
 }
 
 Favourites.prototype.getFavourites = function () {
-    var request = new Request(this.url);
-    request.getRequest(this.onLoad);
+  var request = new Request(this.url);
+  request.getRequest(this.onLoad);
 }
 
 Favourites.prototype.addToFavourites = function (event, correctVideoURL, correctThumbnailURL, dataObject) {
-    event.preventDefault();
-    const body = {
-        title: dataObject.title,
-        description: dataObject.description,
-        href: correctVideoURL,
-        thumbnail: correctThumbnailURL
-    }
-    const createRequestComplete = function () {
-        this.getFavourites();
-    }.bind(this);
-    var request = new Request(this.url);
-    request.post(createRequestComplete, body);
+  event.preventDefault();
+  const body = {
+    title: dataObject.title,
+    description: dataObject.description,
+    href: correctVideoURL,
+    thumbnail: correctThumbnailURL
+  }
+  const createRequestComplete = function () {
+    this.getFavourites();
+  }.bind(this);
+  var request = new Request(this.url);
+  request.post(createRequestComplete, body);
 }
 
 Favourites.prototype.deleteFavourites = function () {
-    var request = new Request(this.url);
-    request.delete();
+  var request = new Request(this.url);
+  request.delete();
 }
 
 
