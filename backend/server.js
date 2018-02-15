bac99f598d6d50bd7a076e2bd4d854b1de8dd3e3
Matt <matthew.r.manson@gmail.com>
2018-02-15 10:54:19 +0000

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -10,65 +10,65 @@ server.use(parser.json());
 server.use(parser.urlencoded({extended:true}));
 
 MongoClient.connect("mongodb://localhost:27017", function (err, client) {
-    if(err){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  const db = client.db('favourites');
+  console.log('Connected to favourites database');
+
+  //GET ALL
+
+  server.get('/api/favourites', function (req, res) {
+    db.collection('videos').find().toArray(function (err, result) {
+      if (err) {
         console.log(err);
-        return;
-    }
+        res.status(500);
+        res.send();
+      }
 
-    const db = client.db('favourites');
-    console.log('Connected to favourites database');
-
-    //GET ALL
-
-    server.get('/api/favourites', function (req, res) {
-        db.collection('videos').find().toArray(function (err, result) {
-            if (err) {
-                console.log(err);
-                res.status(500);
-                res.send();
-            }
-
-            res.status(200)
-            res.json(result);
-            console.log("Database retrieved");
-        });
+      res.status(200)
+      res.json(result);
+      console.log("Database retrieved");
     });
+  });
 
-    //POST
+  //POST
 
-    server.post('/api/favourites', function (req, res) {
-        db.collection('videos').save(req.body, function (err, result) {
-            if(err){
-                console.log(err);
-                res.status(500);
-                res.send();
-            }
-            res.status(201);
-            res.json(result.ops[0]);
+  server.post('/api/favourites', function (req, res) {
+    db.collection('videos').save(req.body, function (err, result) {
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(201);
+      res.json(result.ops[0]);
 
-            console.log("Object Saved")
-        })
+      console.log("Object Saved")
     })
+  })
 
-    //DELETE
+  //DELETE
 
-    server.delete('/api/favourites', function (req, res) {
-        db.collection('videos').deleteMany(function(err){
-            if(err){
-                console.log(err);
-                res.status(500);
-                res.send();
-            }
+  server.delete('/api/favourites', function (req, res) {
+    db.collection('videos').deleteMany(function(err){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
 
-            res.status(204);
-            res.send();
+      res.status(204);
+      res.send();
 
-            console.log('database deleted');
-        });
+      console.log('database deleted');
     });
+  });
 
-    server.listen(5000, function () {
-        console.log('Listening on port 5000');
-    })
+  server.listen(5000, function () {
+    console.log('Listening on port 5000');
+  })
 
 })
